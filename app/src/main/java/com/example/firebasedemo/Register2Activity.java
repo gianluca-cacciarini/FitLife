28b63826b090fc81726461933e157165fbd91918
gianluca-cacciarini <83116078+gianluca-cacciarini@users.noreply.github.com>
2022-10-30 20:24:32 +0100

    changes

--- app/src/main/java/com/example/firebasedemo/Register2Activity.java
+++ app/src/main/java/com/example/firebasedemo/Register2Activity.java
@@ -29,10 +29,7 @@ public class Register2Activity extends AppCompatActivity {
     String txt_email;
     String txt_password;
     User user;
-
-    private int gender = -1;
-    String txt_email, txt_password;
-    User user;
+    
     private CheckBox male, female;
 
     private DatabaseReference mDatabaseReference;
