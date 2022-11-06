df18fd56160b2a609303b972ca22b3d6d7295200
gianluca-cacciarini <83116078+gianluca-cacciarini@users.noreply.github.com>
2022-11-06 16:47:20 +0100

    pageaddedactivity

---
+++ app/src/main/java/com/example/firebasedemo/PedometerPageActivity.java
@@ -0,0 +1,14 @@
+package com.example.firebasedemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PedometerPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pedometer_page);
+    }
+}
