b39c11c2bf07dd6a5accb6b968c7eeedfa362858
gianluca-cacciarini <83116078+gianluca-cacciarini@users.noreply.github.com>
2022-11-20 16:27:24 +0100

    profile_page changes

--- app/src/main/java/com/example/firebasedemo/UserPageActivity.java
+++ app/src/main/java/com/example/firebasedemo/UserPageActivity.java
@@ -7,19 +7,61 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.Collections;
 
 public class UserPageActivity extends AppCompatActivity {
 
     private BottomNavigationView navigationView;
+    private GoogleSignInOptions gso;
+    private GoogleSignInClient gsc;
+    private DatabaseReference mDatabaseReference;
+    private FirebaseAuth mFirebaseAuth;
+    private StorageReference mStorageReference;
+    private TextView logout;
+    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_page);
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(this, gso);
+
+        mStorageReference = FirebaseStorage.getInstance().getReference().child("images");
+        mDatabaseReference = FirebaseDatabase.getInstance("https://fir-demo-5bf06-default-rtdb.europe-west1.firebasedatabase.app").getReference().child("my_app_user");
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        getUser();
+        logout = findViewById(R.id.logout_settings);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("debug","MainActivity 1");
+                signOut();
+            }
+        });
 
         navigationView = findViewById(R.id.navigation_bar);
         navigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
@@ -51,7 +93,75 @@ public class UserPageActivity extends AppCompatActivity {
                 }
                 return true;
             }
+
         });
 
+    }
+
+    public void signOut(){
+        //we get the user values
+        Log.d("debug","MainActivity 2 "+mFirebaseAuth.getCurrentUser().toString());
+        mDatabaseReference.child(mFirebaseAuth.getCurrentUser().getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Log.d("debug", "MainActivity 3");
+                //Toast.makeText(getApplicationContext(), snapshot.getValue().toString(), Toast.LENGTH_SHORT).show();
+                String user_json = snapshot.getValue().toString();
+                user = snapshot.getValue(User.class);
+                Toast.makeText(getApplicationContext(), user.password, Toast.LENGTH_SHORT).show();
+                if (user.password.equals("google_user")) {
+                    Log.d("debug", "MainActivity 4");
+                    GoogleSignOut();
+                }
+                if (user.password.equals("default_user")) {
+                    Log.d("debug", "MainActivity 5");
+                    DefaultSignOut();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+    //signout if the user is not a google user
+    private void DefaultSignOut(){
+        Log.d("debug","MainActivity-->default");
+        FirebaseAuth.getInstance().signOut();
+        Toast.makeText(getApplicationContext(), "Logged Out!", Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(getApplicationContext(), StartActivity.class));
+    }
+
+    //signout for the google users
+    private void GoogleSignOut() {
+        Log.d("debug","MainActivity-->google");
+        gsc.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    // When task is successful
+                    // Sign out from firebase
+                    FirebaseAuth.getInstance().signOut();
+                    GoogleSignIn.getClient(getApplicationContext(), new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).build()).signOut();
+
+                    // Display Toast
+                    Toast.makeText(getApplicationContext(), "Logout successful", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(), StartActivity.class));
+                }
+            }
+        });
+    }
+
+    public void getUser(){
+        mDatabaseReference.child(mFirebaseAuth.getCurrentUser().getUid()).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                DataSnapshot snapshot = task.getResult();
+                user = snapshot.getValue(User.class);
+                //getAllImages();
+                //insertFoodList();
+            }
+        });
     }
 }
